a9244ed59b188146ff5a5a4716d19a3b739bb0a7
Saurabh Kumar <me+github@saurabh-kumar.com>
2015-05-01 10:30:44 +0530

    handle the case where markdown doesn't render section#id

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,11 @@ $(function() {
   return $('.page-single article').find("h2, h3, h4, h5, h6").each(function(i, el) {
     var $el, icon, id;
     $el = $(el);
-    id = $el.parent().attr('id');
+    if($el.attr('id')){
+        id = $el.attr('id');
+    } else {
+        id = $el.parent().attr('id');
+    }
     if (id) {
       return $el.append($("<a />").addClass("header-link icon-link").attr("href", "#" + id).html(""));
     }
